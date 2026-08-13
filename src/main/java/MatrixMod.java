930d34df0dd0191aa85857764aed29fcbf802fa2
kkagado70-cmd <kkagado70@gmail.com>
2026-08-13 01:07:54 -0300

    Update MatrixMod.java

--- src/main/java/MatrixMod.java
+++ src/main/java/MatrixMod.java
@@ -1,4 +1,13 @@
-package com.example;
+package com;
+
+import net.fabricmc.api.ModInitializer;
+
+public class MatrixMod implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        System.out.println("Matrix Client (1.21.x) - Mod Carregado!");
+    }
+}
 
 import net.fabricmc.api.ModInitializer;
 
